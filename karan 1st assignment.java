2a1bdcbab7aa16ad00792abac5b2c05de4c5c955
Karan1586 <109956644+Karan1586@users.noreply.github.com>
2022-07-26 16:28:10 +0530

    ASSIGNMENT 1

---
+++ karan 1st assignment.java
@@ -0,0 +1,104 @@
+/*
+
+class Myclass{
+
+public static void main (){
+ test obj=new test();
+ obj.sum();
+ }
+ void sum(){
+ String name="Karanveer Singh";
+ int age=19;
+ 
+ System.out.printIn("Name="+name);
+ System.out.printIn("Age="+age);
+ 
+   }
+}
+
+*/
+
+  
+ /*
+ 
+
+
+Class name
+Test
+Car
+
+Function name
+test()
+play()
+
+Variable 
+nunChar
+
+Static Variable
+Number
+Speed_Value
+*/
+
+/*
+
+.java files are the files when the user written the program that cam be compiled first are known as .java files 
+
+.class files are the files which are executable because it is already compiled files are known as .class files
+
+
+*/
+
+/*
+
+Java is not a complete OOP,s file because it has primtive values int char which are not objects 
+*/
+
+/*
+
+NR-WR
+
+void name(int a,int b){
+	//code 
+*/
+
+/*
+
+Wrapper classes are the classes which wrapped through primitive data types to provide functions of OOP,s to them
+
+example
+
+Character for char
+Double for double
+
+use integer i=10;
+
+*/
+
+/*
+
+class Calci
+
+static void addy(){
+	System.out.printIn("Methods addy()");
+}
+  void diviji();
+  System.out.printIn("Methods void()");
+  
+  }
+  
+  void mull(){
+	  System.out.printIn("Methods mull()");
+  }
+  
+  Class Calciworking{
+	  
+	  public static void main();
+	  
+	  Calci obj = new calci();
+	  
+	  obj.divji();
+	  obj'mull();
+	  
+  }
+   }
+/*
