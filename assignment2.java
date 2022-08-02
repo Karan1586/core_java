0fe0e5f5fac5fd44098d095a0f4e2be55564ec01
Karan1586 <109956644+Karan1586@users.noreply.github.com>
2022-08-02 16:26:31 +0530

    Create assignment2.java
    assignment 2 submitted

---
+++ assignment2.java
@@ -0,0 +1,182 @@
+1
+//
+import java.util.Scanner;
+class Test
+{
+public static void main(String[] args)
+{
+
+Scanner java=new scanner(System.in);
+
+System.out.println("Value");
+int a=java.nextint();
+
+if (a%2=0){
+System.out.println("Number is even");
+}
+
+else{
+
+System.out.println("Number is odd");
+}
+}
+//
+
+//
+2
+import java.util.Scanner;
+class Karan
+{
+	public static void main(String[] args)
+	{
+		Scanner java=new scanner(System.in);
+		int a=java.nextint();
+		
+		System.out.println("no.is greater or not");
+		if(a>55);
+		
+		System.out.println("No is greater than 55");
+	} else {
+		System.out.println("No is smaller than 55);
+		
+	
+	}
+}
+//  
+
+
+//
+import java.util.Scanner;
+class Hello
+
+public static void main(String[] args)
+	{
+		Scanner java=new scanner(System.in);
+		int a=java.nextint();
+		
+		System.out.println("number 1");
+		int n1=java.nextint();
+		
+		System.out.println("number 2");'
+		int n2=java.nextint();
+		
+		if(n1<n2)
+		{
+			System.out.println("n2 greater than n1");
+	}else if(n1>n2)
+	{
+		System.out.println("n1 greater than n2");
+		
+	}else if(n1=n2)
+	{
+		System.out.println("n1 is equal to n2");
+		
+	}
+	}
+		
+//
+
+
+//
+
+
+import java.util.Scanner;
+class Micro
+{
+
+public static void main(String[] args)
+	{
+		Scanner java=new scanner(System.in);
+		int a=java.nextint();
+		
+		Switch (a)
+		{
+			
+			case(579):
+			System.out.println("Sonu rollno");
+			break;
+			case(546):
+			System.out.println("Monu roll no");
+			break;
+			case(533):
+			System.out.println("Manu roll no");
+			break;
+			case(544):
+			System.out.println("Meenu roll no");
+			break;
+			case(516):
+			System.out.println("meena roll no");
+			break;
+			
+			default:(System.out.println( "No roll no");
+		}
+	}
+	
+	//
+	
+	//
+	
+	PASS BY VALUE:
+	
+	Pass by value is defined as when we send a copy of value of variable to another person then we change or edit a the first copy will be changed but the second copy of value of variable will not changed
+	is known as pass by value.
+	
+	PASS BY REFERENCCE:
+	
+
+	Pass by reference is defined as  when we  change the value of variable also change after method retains
+	//
+	
+	//
+	
+	
+	class You
+	{
+		public static void main(String[] args)
+	{
+		Scanner java=new scanner(System.in);
+		int n=java.nextint();
+		int n=8;
+		for ( int i=1;i<n i++);
+		
+		System.out.println("Value");
+	}
+	}
+	
+	//
+	
+	//
+	
+	
+	class Array
+	{
+		
+		public static void main(String[]  args)
+		
+		{
+			
+	Scanner java=new scanner(System.in);
+		int n=java.addint();
+		
+		int ar[]= new int[10];
+		
+		
+		
+		for ( int i=1;i<10 i++);
+		
+		System.out.println("Value of array");
+		}
+	}
+	
+	//
+	
+	
+	
+		class Destroy
+		{
+			
+			public static void main(String[] args)
+			{
+				
+				
+				
